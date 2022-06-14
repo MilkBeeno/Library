60fcebd2fb3c52c64a83750ae74ef7173e14b884
MilkBeeno <milkbeeno@gmail.com>
2022-06-14 16:31:10 +0800

    状态栏扩展函数

---
+++ support/src/main/java/com/milk/simple/ktx/ScreenSize.kt
@@ -0,0 +1,67 @@
+package com.milk.simple.ktx
+
+import android.app.Activity
+import android.content.res.Configuration
+import android.graphics.Insets
+import android.graphics.Rect
+import android.os.Build
+import android.util.DisplayMetrics
+import android.view.WindowInsets
+
+/**
+ * WindowInsetsCompat.Type.statusBars()      状态栏
+ * WindowInsetsCompat.Type.navigationBars()  底部导航栏
+ * WindowInsetsCompat.Type.captionBar()      标题栏
+ * WindowInsetsCompat.Type.systemBars()      前三者全部
+ **/
+
+
+fun Activity.obtainScreenWidth(): Int {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        val windowMetrics = windowManager.currentWindowMetrics
+        val bounds: Rect = windowMetrics.bounds
+        val insets: Insets = windowMetrics.windowInsets.getInsetsIgnoringVisibility(
+            WindowInsets.Type.systemBars()
+        )
+        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+            && resources.configuration.smallestScreenWidthDp < 600
+        ) {
+            // landscape and phone
+            val navigationBarSize: Int = insets.right + insets.left
+            bounds.width() - navigationBarSize
+        } else {
+            // portrait or tablet
+            bounds.width()
+        }
+    } else {
+        val outMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(outMetrics)
+        outMetrics.widthPixels
+    }
+}
+
+fun Activity.obtainScreenHeight(): Int {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        val windowMetrics = windowManager.currentWindowMetrics
+        val bounds: Rect = windowMetrics.bounds
+        val insets: Insets = windowMetrics.windowInsets.getInsetsIgnoringVisibility(
+            WindowInsets.Type.systemBars()
+        )
+        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+            && resources.configuration.smallestScreenWidthDp < 600
+        ) {
+            // landscape and phone
+            bounds.height()
+        } else {
+            // portrait or tablet
+            val navigationBarSize: Int = insets.bottom
+            bounds.height() - navigationBarSize
+        }
+    } else {
+        val outMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(outMetrics)
+        outMetrics.heightPixels
+    }
+}
+
+
---
+++ support/src/main/java/com/milk/simple/ktx/StatusBar.kt
@@ -0,0 +1,69 @@
+package com.milk.simple.ktx
+
+import android.app.Activity
+import android.graphics.Color
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import androidx.annotation.ColorInt
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
+fun Activity.setStatusBarVisible(isVisible: Boolean) {
+    WindowCompat.setDecorFitsSystemWindows(window, isVisible)
+    WindowInsetsControllerCompat(window, window.decorView).let { controller ->
+        if (isVisible) {
+            controller.show(WindowInsetsCompat.Type.statusBars())
+        } else {
+            controller.hide(WindowInsetsCompat.Type.statusBars())
+        }
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    }
+}
+
+fun Activity.setStatusBarColor(@ColorInt color: Int) {
+    window.statusBarColor = color
+}
+
+fun Activity.setStatusBarDark(dark: Boolean = true) {
+    WindowInsetsControllerCompat(window, window.decorView)
+        .isAppearanceLightStatusBars = dark
+}
+
+fun Activity.obtainStatusBarHeight(): Int {
+    val resourceId: Int = resources
+        .getIdentifier("status_bar_height", "dimen", "android")
+    return if (resourceId > 0) resources.getDimensionPixelSize(resourceId) else 0
+}
+
+fun Activity.immersiveStatusBar(view: View, dark: Boolean = true) {
+    setStatusBarDark(dark)
+    setStatusBarVisible(true)
+    setStatusBarColor(Color.TRANSPARENT)
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+    view.statusBarPadding()
+}
+
+private fun View.statusBarPadding() {
+    try {
+        if (context !is Activity) return
+        val activity = context as Activity
+        layoutParams = when (parent) {
+            is FrameLayout,
+            is LinearLayout,
+            is LinearLayoutCompat,
+            is ConstraintLayout -> {
+                val params = layoutParams as FrameLayout.LayoutParams
+                params.topMargin = activity.obtainStatusBarHeight()
+                params
+            }
+            else -> layoutParams
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
