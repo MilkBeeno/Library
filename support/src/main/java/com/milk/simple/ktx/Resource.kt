8ef102279e5e666907e31339f4bf9fcaa2831f0b
MilkBeeno <milkbeeno@gmail.com>
2022-06-27 10:40:10 +0800

    修改color和string扩展函数

--- support/src/main/java/com/milk/simple/ktx/Resource.kt
+++ support/src/main/java/com/milk/simple/ktx/Resource.kt
@@ -1,17 +1,17 @@
 package com.milk.simple.ktx
 
-import android.app.Activity
+import android.content.Context
 import android.os.Build
 import androidx.annotation.ColorRes
 import androidx.annotation.StringRes
 
-fun Activity.color(@ColorRes resId: Int): Int {
+fun Context.color(@ColorRes resId: Int): Int {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
         resources.getColor(resId, theme)
     else
         resources.getColor(resId)
 }
 
-fun Activity.string(@StringRes resId: Int): String {
+fun Context.string(@StringRes resId: Int): String {
     return resources.getString(resId)
 }
